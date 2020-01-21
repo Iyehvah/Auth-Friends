79811ca6dfcac883e58fb4ffe67da3af397159bc
Iyehvah <Iyehvah@users.noreply.github.com>
2020-01-21 15:36:22 -0600

    Added some basic styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -11,17 +11,11 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/protected">Friends List</Link>
-        </li>
-        <li>
-          <Link to="/newfriend">Add a Friend</Link>
-        </li>
-      </ul>
+        <div className="atags">
+          <Link to="/login" className="tag">Login</Link>
+          <Link to="/protected" className="tag">Friends List</Link>
+          <Link to="/newfriend" className="tag">Add a Friend</Link>
+        </div>
       <Switch>
         <ProtectedRoute exact path="/protected" component={FriendsList} />
         <Route path="/login" component={Login} />
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,5 +1,17 @@
 import React from "react";
 import {GetToken} from "./GetToken";
+import Styled from "styled-components";
+
+
+const FriendDiv = Styled.div`
+    border: 5px solid black;
+    width: 300px;
+    margin: 5px auto 5px auto;
+    padding: 5px;
+    color: black;
+    background-color: teal;
+    font-size: 1.5rem;
+`;
 
 class FriendsList extends React.Component {
     state = {
@@ -25,16 +37,13 @@ class FriendsList extends React.Component {
         return(
             <div>
                 <h2>Friends!</h2>
-
                     {this.state.FriendsList.map(friend => 
-                        <div>
-                            <h3>Name: {friend.name}</h3>
+                        <FriendDiv>
+                            <p>Name: {friend.name}</p>
                             <p>Age: {friend.age}</p>
                             <p>Email: {friend.email}</p>
-                    
-                        </div>
+                        </FriendDiv>
                     )}
-
             </div>
         )
     }
